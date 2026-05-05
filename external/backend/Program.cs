13fc09367d8a7800bc8613e6c7faf91ba9975432
0xAmanita <yldeviermagpusao@gmail.com>
2026-05-06 04:35:46 +0800

    feat(config): register DbContext with PostgreSQL connection

--- external/backend/Program.cs
+++ external/backend/Program.cs
@@ -1,8 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using backend_deob.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+
+// Configure PostgreSQL database
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
